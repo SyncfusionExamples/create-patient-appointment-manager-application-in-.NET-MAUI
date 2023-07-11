737f8c2ebb006db3e4df5c5142be988f8a5fef6b
Jeyasri Murugan <jeyasrim@syncfusion.com>
2023-07-11 13:09:21 +0530

    Sample updated

--- ManageAppointments/ManageAppointments/Model/Appointment.cs
+++ ManageAppointments/ManageAppointments/Model/Appointment.cs
@@ -3,46 +3,91 @@ using Syncfusion.Maui.Scheduler;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Resources;
 
 namespace ManageAppointments
 {
-    public class Appointment
+    public class Appointment : INotifyPropertyChanged
     {
+        private DateTime from;
+        private DateTime to;
+        private bool isAllDay;
+        private string eventName;
+        private Brush background;
+        public string location;
         public Appointment()
         {
-            this.From = DateTime.Now;
-            this.To = DateTime.Now;
-            this.EventName = string.Empty;
-            this.IsAllDay = false;
-            this.Background = Brush.Transparent;
+            this.from = DateTime.Now;
+            this.to = DateTime.Now;
+            this.eventName = string.Empty;
+            this.isAllDay = false;
+            this.background = Brush.Transparent;
         }
 
         /// <summary>
         /// Gets or sets the value to display the start date.
         /// </summary>
-        public DateTime From { get; set; }
+        public DateTime From
+        {
+            get
+            { return this.from; }
+            set { this.from = value;
+                this.OnPropertyChanged(nameof(From));
+            }  
+        }
 
         /// <summary>
         /// Gets or sets the value to display the end date.
         /// </summary>
-        public DateTime To { get; set; }
+        public DateTime To
+        {
+            get { return this.to; } 
+            set { this.to = value;
+                this.OnPropertyChanged(nameof(To));
+            }   
+        }
 
         /// <summary>
         /// Gets or sets the value indicating whether the appointment is all-day or not.
         /// </summary>
-        public bool IsAllDay { get; set; }
+        public bool IsAllDay
+        {
+            get { return this.isAllDay; }
+            set { this.isAllDay = value;
+                this.OnPropertyChanged(nameof(IsAllDay));
+            }
+        }
 
         /// <summary>
         /// Gets or sets the value to display the subject.
         /// </summary>
-        public string EventName { get; set; }
+        public string EventName
+        {
+            get { return this.eventName; }
+            set { this.eventName = value; }
+        }
 
         /// <summary>
         /// Gets or sets the value to display the background.
         /// </summary>
-        public Brush Background { get; set; }
+        public Brush Background
+        {
+            get { return this.background; }
+            set { this.background = value; }
+        }
 
-        public string Location { get; set; }
+        public string Location
+        {
+            get { return this.location; }
+            set { this.location = value; }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
     }
 }
